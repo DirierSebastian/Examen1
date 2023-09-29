6e6d3decbb8ce95ed5429b751beb9a09b88ddda7
DirierSebastian <sebastianagvzla@gmail.com>
2023-09-29 09:04:58 -0700

    Agregar archivos de proyecto.

---
+++ Examen1/Examen.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace Examen1
+{
+    internal class Examen
+    {
+        public string Numeros(int inicio, int final)
+        {
+            if (final <= inicio)
+            {
+                Console.WriteLine("La secuencia de los números es " + inicio);
+                if (inicio == final)
+                {
+                    return "Los números son iguales";
+                }
+                return Numeros(--inicio, final);
+            }
+            else
+            {
+                Console.WriteLine("La secuencia de los números es " + inicio);
+                if (inicio == final)
+                {
+                    return "Los números son iguales";
+                }
+                return Numeros(++inicio, final);
+
+            }
+
+        }
+        public string Numeros2(int inicio, int final)
+        {
+            while (final <= inicio)
+            {
+                Console.WriteLine("La secuencia de los números es " + inicio);
+                if (inicio == final)
+                {
+                    return "Los números son iguales";
+                }
+                return Numeros(--inicio, final);
+            }
+            while(final <= inicio)
+            {
+                Console.WriteLine("La secuencia de los números es " + inicio);
+                if (inicio == final)
+                {
+                    return "Los números son iguales";
+                }
+                return Numeros(++inicio, final);
+
+            }
+            if (inicio == final)
+            {
+                return "El numero es igual";
+            }
+            else
+            {
+                return Numeros2(inicio, final);
+            }
+
+        }
+        public string Tablas(int inicio, int final, int tabla)
+        {
+            if (tabla != inicio && inicio != final)
+            {
+                Console.WriteLine($"{tabla} x {inicio} = {tabla * inicio}");
+                return Tablas(++inicio, final, tabla);
+            }
+            if (tabla ==  inicio && inicio == final)
+            {
+                Console.WriteLine($"{tabla} x {inicio} = {tabla * inicio}");
+                return "Has llegado al final";
+            }
+            if( inicio != final)
+            {
+                Console.WriteLine($"{tabla} x {inicio} = {tabla * inicio}");
+                return Tablas(++inicio, final, tabla);
+            }
+            if (final == inicio)
+            {
+                Console.WriteLine($"{tabla} x {inicio} = {tabla * inicio}");
+
+                return Tablas(inicio-9, final, ++tabla);
+            }
+            return "";
+        }
+
+    }
+}
---
+++ Examen1/Program.cs
@@ -0,0 +1,11 @@
+﻿using Examen1;
+
+Examen examen = new Examen();
+/*Console.WriteLine("Ingrese un número de inicio");
+int inicio = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Ingrese un número final");
+int final =  Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(examen.Numeros(inicio, final)); */
+Console.WriteLine("Ingresa una tabla del uno al diez");
+int tabla = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(examen.Tablas(0, 10, tabla));
